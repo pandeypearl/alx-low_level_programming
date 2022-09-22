1b2460bb9fc17cc56dcee31c36f836b21cd63564
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-22 15:08:49 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,15 +5,13 @@
 *
 * Description: using the main function
 *
-this program prints "Programming is like
-building a multilingual puzzle"
-*Return: 0
+this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
 	puts("\"
-Programming is like building a multilingual
-puzzle");
-	return(0);
+Programming is like building a multilingual puzzle");
+	return (0);
 }
 
