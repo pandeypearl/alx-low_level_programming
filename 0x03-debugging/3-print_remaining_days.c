830a5738c0a43882c7a17346c00ff55e8efd79a4
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-23 13:44:21 -0700

    Like commnt subscribe

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,6 +8,7 @@
 *@year: year
 *Return: void
 */
+
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
