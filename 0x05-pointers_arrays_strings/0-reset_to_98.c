261f4010246127f4f4c4989c41f1f4bca28403a0
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-24 09:56:52 -0700

    98 battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*reset_to_98 - function that takes a pointer to an int as parameter up to 98
+*@n: declaration of n and paramters for the function reset_to_98
+*Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void reset_to_98(int *n);
+
+#endif
