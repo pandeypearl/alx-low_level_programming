ca0ecad40a83fd13cd1b1c5bd26122fa3c893f33
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-24 08:30:27 -0700

    Numbers have life

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - function to print integer
+*Return: 0
+*/
+
+int main(void)
+{
+	printf("Enter a integer: ");
+	scanf("%d", &number);
+
+	printf("You entered: %d", number);
+	return (0);
+}
+
