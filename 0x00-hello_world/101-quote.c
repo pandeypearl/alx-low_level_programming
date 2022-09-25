5e057bda703a7396eea2cb79050b3b5f20605176
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-25 04:34:48 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 /**
  *main - main function
  *
- *Return: 1
+ *Return: 0
  */
 
 int main(void)
 {
-	char *string;
-	char str1[] = "and that piece of art is useful\"";
-	char str2[] = "- Dora Korpar, 2015-10-2019";
-
-	string = malloc(strlen(str1) + strlen(str2) + 1);
-	strcpy(string, str1);
-	strcpy(string, str2);
-	write(2, string, sizeof(string));
-
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
