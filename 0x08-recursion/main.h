db60023d9cc43ead5d3110cbd57ecbbd43cac103
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-28 12:37:06 -0700

    8. Inception

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -11,5 +11,8 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int full_compare(char*, char*, char*, char *);
+int find_next_c(char**, char*);
+int find_after_sc(char**, char*,char);
 
 #endif
