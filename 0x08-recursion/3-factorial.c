2629c3297bf742b936187720d9e2d5cec389190b
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-28 01:36:15 -0700

    Fixed typo error

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,11 +6,11 @@
  * Return: factorial of n
  */
 
-int factoral(int n)
+int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
-	return (n * factoral(n - 1));
+	return (n * factorial(n - 1));
 }
