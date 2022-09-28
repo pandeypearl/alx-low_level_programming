6b4a98b9e9011279091a624e13cb6559dd96ef69
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-28 04:04:46 -0700

    Inception

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -57,10 +57,11 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 	{
 		s2 = move(s2);
+
 		if (*s2 == '\0')
 			return (1);
 		if (*s1 == *s2)
-			sum += wildcpm(s1 + 1, s2 + 1);
+			sum += wildcmp(s1 + 1, s2 + 1);
 		sum += bandersnatch(s1 + 1, s2);
 		return (!!sum);
 	}
