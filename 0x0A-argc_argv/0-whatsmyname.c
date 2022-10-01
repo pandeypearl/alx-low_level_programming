8e880d6d13bedb5ca679f22f48f1be1031ac2dc4
Pretty Pandey <prettypandeypearl@gmail.com>
2022-09-30 19:38:15 -0700

    fixed whitespace error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 
+ * Return: 0
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
