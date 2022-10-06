84fed72db25baeaf5f2e4fc04fc4c34f2d75ebd0
Pretty Pandey <prettypandeypearl@gmail.com>
2022-10-06 10:47:08 -0700

    1. string_nconcat fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0; j = 0; len1 = 0; len2 = 0;
+i	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -21,11 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n < len2)
 		s = malloc(sizeof(char) * (len1 + n + 1));
-	else 
+	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (!s)
-		return NULL;
+		return (NULL);
 
 	while (i < len1)
 	{
@@ -42,4 +42,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s[i] = '\0';
 
 	return (s);
-}	
+}
