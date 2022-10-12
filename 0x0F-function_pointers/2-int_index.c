b606d1d87c77bec21729ef893a4da92e40e7f7c8
Pretty Pandey <prettypandeypearl@gmail.com>
2022-10-12 07:08:33 -0700

    code fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
@@ -15,13 +14,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array && cmp)
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
-		{
-			if (cmp(array[i] != 0)
-				return (i);
-		}
+		if (cmp(array[i] != 0)
+			return (i);
 	}
 
 	return (-1);
