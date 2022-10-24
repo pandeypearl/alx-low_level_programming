4d7959f84c7845a7d7144eddf96c86f7df3d08cb
Pretty Pandey <prettypandeypearl@gmail.com>
2022-10-24 13:59:01 -0700

    code fix

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,19 +12,15 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL, *next = NULL;
 
-	if (head)
+	while (head)
 	{
-		while (head)
-		{
-			next = *head;
-			*head = (*head)->next;
-			next->next = prev;
-			prev = next;
-		}
-
-		*head = prev;
-		return (*head);
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
-	return (NULL);
+	*head = prev;
+
+	return (*head);
 }
