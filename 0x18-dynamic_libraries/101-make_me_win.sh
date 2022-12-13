2d356329efcfbc821442cfe6827908d9f36c7792
Pretty Pandey <prettypandeypearl@gmail.com>
2022-12-13 01:16:22 -0800

    code fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -O /tmp/libgiga.so https://github.com/pandeypearl/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -P .. https://raw.githubusercontent.com/pandeypearl/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
+
