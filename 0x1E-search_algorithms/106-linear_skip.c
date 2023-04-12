b56dad9775c364da2ba8de62519660f7f077791a
Pretty Pandey <prettypandeypearl@gmail.com>
2023-04-12 08:59:50 -0700

    Linear search in a skip list

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -20,7 +20,8 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		{
 			node = tmp->express;
 			printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
-		} else
+		}
+		else
 		{
 			node = tmp;
 			while (node->next)
@@ -29,7 +30,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 
 		if (value <= node->n || !(tmp->express))
 		{
-			printf("Value found between indexes [%ld} and [%ld}\n",
+			printf("Value found between indexes [%ld] and [%ld]\n",
 					tmp->index, node->index);
 			old_index = node->index;
 			for (node = tmp; node && node->index <= old_index; node = node->next)
